f57b4fe3f5001c7a5bea7cda6711337db7c4e400
Jesse Roux <roux.jesse@gmail.com>
2018-09-28 15:36:42 -0400

    Test serializer create and update

--- user/tests/serializer.py
+++ user/tests/serializer.py
@@ -8,6 +8,9 @@ from ..serializers import UserSerializer
 
 
 class TestSerializer(TestCase):
+  def setUp(self):
+    User.objects.all().delete()
+
   def test_serialize_user_instance(self):
     u = User.objects.create(
       username="test_user",
@@ -21,3 +24,31 @@ class TestSerializer(TestCase):
       "email": "test@test.com",
       "date_joined": re.sub(r"\+.*", "Z", u.date_joined.isoformat())
     })
+
+  def test_create_user(self):
+    u = UserSerializer().create({
+      "username": "test_user",
+      "email": "test@test.com",
+      "password": "testpass"
+    })
+
+    self.assertEqual(u, User.objects.get(username="test_user"))
+    self.assertTrue(u.check_password("testpass"))
+
+  def test_update_user(self):
+    u = User(username="test_user", email="test@test.com")
+    u.set_password("password123")
+    u.save()
+
+    user_id = u.id
+
+    UserSerializer().update(u, {
+      "username": "new",
+      "email": "new@test.com",
+      "password": "NewPassword"
+    })
+
+    self.assertEqual(u.id, user_id)
+    self.assertEqual(u.username, "new")
+    self.assertEqual(u.email, "new@test.com")
+    self.assertTrue(u.check_password("NewPassword"))
