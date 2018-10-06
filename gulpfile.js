8bba580705567a2036e8193d484d25a76209d12b
Jesse Roux <roux.jesse@gmail.com>
2018-10-06 00:43:47 -0400

    Cleanup gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,60 @@ const Autoprefixer = require("gulp-autoprefixer");
 const Sass = require("gulp-sass");
 
 
-function buildStylesheets(prod) {
-  return Gulp.src("./assets/css/*.scss")
-    .pipe(Sass(prod ? { outputStyle: "compressed" } : null)
+const src = "app/static/";
+const dst = "build/";
+const paths = {
+  css: {
+    src: src + "css/style.scss",
+    dst: dst
+  },
+
+  fonts: {
+    src: src + "fonts/**/*.@(woff|woff2|eot|ttf|otf)",
+    dst: dst + "fonts/"
+  },
+
+  images: {
+    src: src + "img/**/*.@(png|jpg|svg|gif)",
+    dst: dst + "img/"
+  }
+}
+
+
+Gulp.task("images", function() {
+  return Gulp.src(paths.images.src).pipe(Gulp.dest(paths.images.dst));
+});
+
+Gulp.task("images:watch", function() {
+  Gulp.watch(paths.images.src, ["images"]);
+});
+
+Gulp.task("fonts", function() {
+  return Gulp.src(paths.fonts.src).pipe(Gulp.dest(paths.fonts.dst));
+});
+
+Gulp.task("fonts:watch", function() {
+  Gulp.watch(paths.fonts.src, ["fonts"]);
+});
+
+Gulp.task("scss", function() {
+  return Gulp.src(paths.css.src)
+    .pipe(Sass({
+      outputStyle: "compressed",
+      includePaths: [ "node_modules"],
+    })
       .on("error", Sass.logError))
     .pipe(Autoprefixer({
       cascade: false,
       browsers: ["last 2 versions", "> 1%"]
     }))
-    .pipe(Gulp.dest("dist/"));
-}
-
-Gulp.task("scss", function() {
-  buildStylesheets(false);
+    .pipe(Gulp.dest(paths.css.dst));
 });
 
-Gulp.task("scss:prod", function() {
-  buildStylesheets(true);
+Gulp.task("scss:watch", function () {
+  Gulp.watch(paths.css.src, ["scss"]);
 });
 
-Gulp.task('scss:watch', function () {
-  Gulp.watch("assets/css/**/*.scss", ['scss']);
-});
 
-Gulp.task("default", ["scss:prod"]);
-Gulp.task("watch", ["scss", "scss:watch"]);
+Gulp.task("default", ["scss", "images", "fonts"]);
+Gulp.task("watch", ["scss", "images", "fonts", "scss:watch", "images:watch", "fonts:watch"]);
